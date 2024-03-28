8de54537923064782163527c81e6f02185ae8a45
gabrielnoah129@gmail.com <Geff115>
2024-03-28 10:51:17 +0100

    Working on our main function

---
+++ main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - The starting point of our program. This function
+ * handles the monty bytecodes to push, pop, swap, add, or even
+ * print all elements on the stack.
+ * @head: A pointer to the first node of the list.
+ *
+ * Return: Always (0) success.
+ */
+int main(int argc, char *argv)
+{
+	stack_t *head = NULL;
+
+	while (arg
